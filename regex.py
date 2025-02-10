39dd6531b32dff6e04fbdac55c74b876c16c0648
Kigula Jesse <jkigula@kolapro.com>
2025-02-10 13:29:17 +0300

    regex modified

--- regex.py
+++ regex.py
@@ -22,6 +22,32 @@ new_string = re.sub(pattern,replacement,text)
 print("")
 print("New String: ",new_string)
 
+#Splitting Strings
+split = r"[with]"
+new_split = re.split(split, text)
+print("")
+print("Splitting Strings")
+print(new_split)
+
+
+#Matching Strings
+text = "Testing regex with Kigula by Kigula"
+pattern = r"Kigula"
+match = re.match(pattern, text)
+if match:
+    print("")
+    print("Match Strings")
+    print(match.group())
+
+text = "Hello World"
+pattern = r"Hello"
+match = re.match(pattern, text)
+if match:
+    print("Match found:", match.group())  # Output: Hello
+
+
+
+
 
 
 
