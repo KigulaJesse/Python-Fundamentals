8685f3112c7435bf27210aa2d0229840a753da9e
Kigula Jesse <jkigula@kolapro.com>
2025-02-13 10:01:02 +0300

    voices

--- speechify_api.py
+++ speechify_api.py
@@ -1,3 +1,15 @@
+import requests
 
+url = "https://api.sws.speechify.com/"
+api_key = "GPvah_Ak2J2kvcdEPX3ZuWQTIjxB7JbvyH2Ruu3_3t4="
+headers = {
+    "Authorization": f"Bearer {api_key}",
+    "Content-Type": "application/json"
+}
 
-
+#Simple Get voices
+# response = requests.get(f"{url}/v1/voices", headers=headers)
+# for voice in response.json():
+#     print("")
+#     print(voice)
+    
--- tests.py
+++ tests.py
@@ -1,17 +1,28 @@
 import re
 
 # pattern = re.compile(r"<(/?)(\w+)([^>]*)>")
-pattern = re.compile(r"<(/?)(\w+)([^>]*)>")
+# pattern = re.compile(r"<(/?)(\w+)([^>]*)>")
 
-# Example HTML-like string
-text = """
-    <div class='container' name="james" id="james">
-        Content
-        <br/>
-    </div>
-"""
+# # Example HTML-like string
+# text = """
+#     <div class='container' name="james" id="james">
+#         Content
+#         <br/>
+#     </div>
+# """
 
-matches = pattern.findall(text)
+# matches = pattern.findall(text)
 
-for match in matches:
-    print(match)
+# for match in matches:
+#     print(match)
+
+
+import requests
+
+
+x = requests.get("https://w3schools.com/python/demopage.htm")
+
+print(x.json)
+print(x.text)
+
+
